baca91d5791d2b4498573b729402052465ba793f
Aykan8106 <turanaykan06@gmail.com>
2026-09-18 00:06:31 +0300

    run.py dolduruldu

--- hello.py
+++
@@ -1,4 +0,0 @@
- from flask 
- import Flask app = Flask(__name__) @app.route('/')
-  def anasayfa(): return '<h1>SmartLead AI calisiyor!</h1>' 
-  if __name__ == '__main__': app.run()
---
+++ run.py
@@ -0,0 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def anasayfa():
+    return '<h1>SmartLead AI calisiyor!</h1>'
+
+if __name__ == '__main__':
+    app.run()
