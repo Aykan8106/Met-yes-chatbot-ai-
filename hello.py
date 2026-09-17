b53acc26b840328a3b840ffdaa51e05dc4fee9e4
Aykan8106 <turanaykan06@gmail.com>
2026-09-18 00:01:04 +0300

    test

--- hello.py
+++ hello.py
@@ -1,7 +1,4 @@
-from flask import Flask
-app = Flask(__name__)
-@app.route('/')
-def merhaba():
-    return 'Ortam calisiyor!'
-if __name__ == '__main__':
-    app.run(port=5000)
+ from flask 
+ import Flask app = Flask(__name__) @app.route('/')
+  def anasayfa(): return '<h1>SmartLead AI calisiyor!</h1>' 
+  if __name__ == '__main__': app.run()
